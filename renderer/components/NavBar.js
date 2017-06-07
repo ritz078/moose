7d57f1eb6be9d48693353efbc1ff083a46b06a4f
Ritesh Kumar <rkritesh078@gmail.com>
2017-06-08 01:56:11 +0530

    feat(badge): animated download count badge

--- renderer/components/NavBar.js
+++ renderer/components/NavBar.js
@@ -2,11 +2,14 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Ink from 'react-ink';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Router from 'next/router';
+import NotificationBadge, { Effect } from 'react-notification-badge';
 import Link from 'next/link';
 import { isEmpty } from 'lodash';
 import { readConfig } from 'snape-config';
 import Cast from './Cast';
+import colors from '../constants/colors';
 
 const Wrapper = styled.div`
   height: 40px;
@@ -18,8 +21,7 @@ const Wrapper = styled.div`
 
 const Icon = styled.i`
   font-size: 20px;
-  padding: 8px 10px;
-  vertical-align: middle;
+  padding: 8px 14px 6px 14px;
   display: inline-block;
   color: #777;
   cursor: pointer;
@@ -27,6 +29,12 @@ const Icon = styled.i`
   &:hover{
     color: #555;
   }
+  
+  ${props =>
+    props.active &&
+    css`
+    color: ${colors.primary}
+  `}
 `;
 
 export default class MenuBar extends PureComponent {
@@ -47,17 +55,35 @@ export default class MenuBar extends PureComponent {
     });
   }
 
+  isCurrentRoute = path => path === Router.pathname;
+
   render() {
+    const badgeContainerStyle = {
+      position: 'absolute',
+      top: 1,
+      right: 1,
+    };
+
+    const badgeStyle = {
+      backgroundColor: colors.primary,
+    };
+
     return (
       <Wrapper>
         <Cast />
         <Link href="/download" prefetch>
-          <Icon className="mdi mdi-download badge" data-badge={this.props.downloads.length}>
+          <Icon className="mdi mdi-download" active={this.isCurrentRoute('/download')}>
+            <NotificationBadge
+              count={this.props.downloads.length}
+              containerStyle={badgeContainerStyle}
+              style={badgeStyle}
+              effect={Effect.ROTATE_Y}
+            />
             <Ink />
           </Icon>
         </Link>
         <Link href="/" prefetch>
-          <Icon className="mdi mdi-home"><Ink /></Icon>
+          <Icon className="mdi mdi-home" active={this.isCurrentRoute('/')}><Ink /></Icon>
         </Link>
       </Wrapper>
     );

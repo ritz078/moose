b660bfc46a702325eef71e45c5276a436d8779cb
Ritesh Kumar <rkritesh078@gmail.com>
2017-08-16 20:34:42 +0530

    fix(main): check if error is string
    fixes #31

--- main/index.js
+++ main/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, autoUpdater } = require('electron');
 const getPort = require('get-port');
 const fixPath = require('fix-path');
 const dev = require('electron-is-dev');
@@ -13,8 +13,15 @@ if (dev) {
   require('electron-debug')();
 }
 
+if (!dev) {
+  const updateServer = 'https://hazel-xzdrgzprqt.now.sh';
+  const feed = `${updateServer}/update/${process.platform}/${app.getVersion()}`;
+
+  autoUpdater.setFeedURL(feed);
+}
+
 process.on('unhandledRejection', (error) => {
-  if (!error.includes('Database maintenance')) {
+  if (error && typeof error === 'string' && !error.includes('Database maintenance')) {
     logError(error);
   }
 });

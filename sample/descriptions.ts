72739fd2605fbdaaef365f78781fd614d2b330ac
Ritesh Kumar <ritz078@users.noreply.github.com>
2020-06-15 01:42:33 +0530

    chore(sample): decrease the number of samples (#77)
    closes #73

--- sample/descriptions.ts
+++ sample/descriptions.ts
@@ -2,45 +2,6 @@ import { ITorrentDescription } from "../types/TorrentDescription";
 
 export const sampleDescriptions: ITorrentDescription[] = [
   {
-    title: "Big Buck Bunny",
-    year: "2008",
-    rated: "TV-Y7",
-    released: "10 Apr 2008",
-    runtime: "10 min",
-    genre: "Animation, Short, Comedy",
-    description:
-      "A recently awoken enormous and utterly adorable fluffy rabbit is heartlessly harassed by a flying squirrel's gang of rodents who are determined to squash his happiness.",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNjRjYjRhNmQtNWE0YS00NWIwLWFhYjUtMTkzZTUwYTE4MTBiXkEyXkFqcGdeQXVyNjA3OTI5MjA@._V1_SX300.jpg",
-    imdbID: "tt1254207",
-  },
-  {
-    title: "Cosmos Laundromat",
-    year: "2015",
-    rated: "N/A",
-    released: "10 Aug 2015",
-    runtime: "12 min",
-    genre: "Animation, Short, Fantasy, Sci-Fi",
-    description:
-      "On a desolate island, suicidal sheep Franck meets his fate in a quirky salesman, who offers him the gift of a lifetime. Little does he know that he can only handle this much 'lifetime'.",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BODUwMjE4OTQ2Nl5BMl5BanBnXkFtZTgwMDE3NjQ2NjE@._V1_SX300.jpg",
-    imdbID: "tt4957236",
-  },
-  {
-    title: "Sintel",
-    year: "2010",
-    rated: "N/A",
-    released: "27 Sep 2010",
-    runtime: "14 min",
-    genre: "Animation, Short, Fantasy",
-    description:
-      "The film follows a girl named Sintel who is searching for a baby dragon she calls Scales. A flashback reveals that Sintel found Scales with its wing injured and helped care for it, forming ...",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMzc5NTUzNTgzMF5BMl5BanBnXkFtZTcwODcwMzQ5Mw@@._V1_SX300.jpg",
-    imdbID: "tt1727587",
-  },
-  {
     title: "Tears of Steel",
     year: "2012",
     rated: "N/A",
